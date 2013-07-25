95595c889d447c2b4656408ca2b68b0b751e3d88
root <root@ubuntu.(none)>
2013-07-25 16:49:46 -0500

    latest postrun script

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@ echo "running setup"
 cd /opt
 
 #pull any repo's I want by default add as needed
-git clone http://github.com/CiscoSystems/grizzly-manifests -b multi-node
 rm -rf /opt/grizzly-manifests
+git clone http://github.com/CiscoSystems/grizzly-manifests -b multi-node
 cp /opt/grizzly-manifests/manifests/* /etc/puppet/manifests
-cp /opt/grizzly-manifests/templates /etc/puppet/templates
+cp /opt/grizzly-manifests/templates/* /etc/puppet/templates
 
 #copy the site.pp in place
 cp /opt/openstack-working/site/site.pp /etc/puppet/manifests
