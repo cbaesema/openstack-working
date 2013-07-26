86180a105c3947f1966ccd9e957582dcd5432271
root <root@ubuntu.(none)>
2013-07-25 23:28:14 -0500

    fix script strip

--- setproposed.py
+++ setproposed.py
@@ -7,9 +7,9 @@ fp.close()
 fp = open('/etc/puppet/manifests/puppet_modules.py','w')
 for line in lines:
     if line.startswith('REPO_NAME'):
-       fp.write(line.replace('grizzly','grizzly-proposed').strip() + "\n")
+       fp.write(line.replace('grizzly','grizzly-proposed') + "\n")
     else:
-        fp.write(line.strip() + "\n")
+        fp.write(line + "\n")
 fp.close()
          
              
@@ -18,9 +18,9 @@ lines = fp.readlines()
 fp.close()
 fp = open('/etc/puppet/manifests/core.pp','w')
 for line in lines:
-    if line.strip().startswith('release'):
-       fp.write(line.replace('grizzly','grizzly-proposed').strip() + "\n")
+    if 'release' in line:
+       fp.write(line.replace('grizzly','grizzly-proposed') + "\n")
     else:
-        fp.write(line.strip() + "\n")
+        fp.write(line + "\n")
 fp.close()
         
