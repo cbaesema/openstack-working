6a4bf558d2ef01e2002f8b29c835f4078b5021fe
root <root@ubuntu.(none)>
2013-10-16 22:01:47 -0500

    modify setup

--- setup.sh
+++ setup.sh
@@ -5,9 +5,9 @@ cd /opt
 
 #pull any repo's I want by default add as needed
 rm -rf /opt/grizzly-manifests
-git clone http://github.com/CiscoSystems/grizzly-manifests -b multi-node
-cp /opt/grizzly-manifests/manifests/* /etc/puppet/manifests
-cp /opt/grizzly-manifests/templates/* /etc/puppet/templates
+git clone http://github.com/CiscoSystems/openstack-installer 
+#cp /opt/grizzly-manifests/manifests/* /etc/puppet/manifests
+#cp /opt/grizzly-manifests/templates/* /etc/puppet/templates
 
 cp /opt/openstack-working/bin/* /usr/sbin
 
