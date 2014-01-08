f116da0e00f429cda31948cff3d2b0b69a531a8f
root <root@node01.eau.wi.charter.com>
2014-01-08 11:01:38 -0600

    compressed

--- setup.sh
+++ setup.sh
@@ -4,10 +4,8 @@ echo "running setup"
 cd /opt
 
 #pull any repo's I want by default add as needed
-rm -rf /opt/grizzly-manifests
-git clone http://github.com/CiscoSystems/grizzly-manifests -b multi-node
-cp /opt/grizzly-manifests/manifests/* /etc/puppet/manifests
-cp /opt/grizzly-manifests/templates/* /etc/puppet/templates
+rm -rf /opt/puppet_openstack_builder
+git clone http://github.com/CiscoSystems/puppet_openstack_builder -b compressed-ha
 
 cp /opt/openstack-working/bin/* /usr/sbin
 
