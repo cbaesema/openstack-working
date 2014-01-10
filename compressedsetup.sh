34273474f2d0a3a6e4626d5875b8b37235fa4b4d
root <root@node01.eau.wi.charter.com>
2014-01-09 19:48:07 -0600

    fixes

--- compressedsetup.sh
+++ compressedsetup.sh
@@ -2,4 +2,4 @@
 cp -r /opt/puppet_openstack_builder/data/* /etc/puppet/data
 cp config.yaml /etc/puppet/data
 cp user.compressed_ha.yaml /etc/puppet/data/hiera_data
-
+cp /opt/puppet-openstack-ha/* /usr/share/puppet/modules/openstack-ha
