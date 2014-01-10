077074b5640a791cab551b7bbf9e2ec759bf3202
root <root@ubuntu.(none)>
2014-01-09 19:28:55 -0600

    ch

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ cd /opt
 #pull any repo's I want by default add as needed
 rm -rf /opt/puppet_openstack_builder
 git clone http://github.com/CiscoSystems/puppet_openstack_builder -b compressed-ha
+rm -rf /opt/puppet-openstack-ha
+git clone http://github.com/cbaesema/puppet-openstack-ha -b havana
 
 cp /opt/openstack-working/bin/* /usr/sbin
 
