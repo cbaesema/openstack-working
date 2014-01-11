63909d236014ad74270c9466eefe4ddd96200c9e
root <root@node01.eau.wi.charter.com>
2014-01-10 19:49:40 -0600

    new setup

--- compressed.sh
+++ compressed.sh
@@ -3,4 +3,4 @@ hostname node01
 export scenario=compressed_ha
 export vendor=cisco
 export vendor_repo=cisco
-export vendor_branch=compressed-ha
+export vendor_branch=coi-development
--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ cd /opt
 
 #pull any repo's I want by default add as needed
 rm -rf /opt/puppet_openstack_builder
-git clone http://github.com/CiscoSystems/puppet_openstack_builder -b compressed-ha
+git clone http://github.com/CiscoSystems/puppet_openstack_builder -b coi-development
 rm -rf /opt/puppet-openstack-ha
 git clone http://github.com/cbaesema/puppet-openstack-ha -b havana
 
