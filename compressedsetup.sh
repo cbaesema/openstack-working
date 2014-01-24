f7557a08d0bd20ee0a41174e8aa933461244f4f1
root <root@node01.eau.wi.charter.com>
2014-01-24 08:29:27 -0600

    remove ha

--- compressedsetup.sh
+++ compressedsetup.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 cp -r /opt/puppet_openstack_builder/data/* /etc/puppet/data
 cp config.yaml /etc/puppet/data
-cp user.compressed_ha.yaml /etc/puppet/data/hiera_data
-cp -r /opt/puppet-openstack-ha/* /usr/share/puppet/modules/openstack-ha
