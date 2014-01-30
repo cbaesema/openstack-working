e030cecd4ae32ca874084eedd42ed5f42cce1f83
root <root@all-in-one.domain.name>
2014-01-29 20:09:25 -0600

    add working

--- aio.sh
+++ aio.sh
@@ -4,7 +4,7 @@ export scenario=all_in_one
 export vendor=cisco
 export vendor_repo=cisco
 export vendor_branch=coi-development
-cp /opt/openstack-working/cisco.install.sh /opt/puppet_openstack_builder/install-scripts
+#cp /opt/openstack-working/cisco.install.sh /opt/puppet_openstack_builder/install-scripts
 
 rm /opt/puppet_openstack_builder/data/hiera_data/vendor/cisco_coi_common.yaml
 
